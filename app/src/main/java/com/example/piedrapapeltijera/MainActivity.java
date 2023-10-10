c5c4e319eb1ef458acfeadd93bbeace68ea1101c
garbettbrandon <garbettbrandon@gmail.com>
2023-10-10 21:32:19 +0200

    Actualizando Proyecto

--- app/src/main/java/com/example/piedrapapeltijera/MainActivity.java
+++ app/src/main/java/com/example/piedrapapeltijera/MainActivity.java
@@ -1,39 +1,70 @@
 package com.example.piedrapapeltijera;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Random;
+import com.example.piedrapapeltijera.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    TextView resultado, resultadoYo, resultadoM;
-    ImageButton piedra, papel, tijera;
-    Button reset;
-    Player jugador;
-    IAMaquina maquina;
+    ImageButton btnPiedra, btnPapel, btnTijeras;
+    TextView txtResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        jugador = new Player(0);
-        maquina = new IAMaquina(0);
+        btnPiedra = findViewById(R.id.btnPiedra);
+        btnPapel = findViewById(R.id.btnPapel);
+        btnTijeras = findViewById(R.id.btnTijeras);
+        txtResultado = findViewById(R.id.tvResultado);
 
-        resultado=findViewById(R.id.resultado);
-        resultadoYo=findViewById(R.id.marcadorYo);
-        resultadoM=findViewById(R.id.marcadorM);
-        piedra=findViewById(R.id.botonPiedra);
-        papel=findViewById(R.id.botonPapel);
-        tijera=findViewById(R.id.botonTijera);
-        reset=findViewById(R.id.botonReset);
+        btnPiedra.setOnClickListener(this);
+        btnPapel.setOnClickListener(this);
+        btnTijeras.setOnClickListener(this);
+    }
 
-        Manejador manejador= new Manejador (resultado, resultadoYo, resultadoM, jugador, maquina);
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnPiedra:
+                jugar("Piedra");
+                break;
+            case R.id.btnPapel:
+                jugar("Papel");
+                break;
+            case R.id.btnTijeras:
+                jugar("Tijeras");
+                break;
+        }
+    }
 
-        piedra.setOnClickListener(manejador);
-        papel.setOnClickListener(manejador);
-        tijera.setOnClickListener(manejador);
+    private void jugar(String eleccionJugador) {
+        String[] opciones = {"Piedra", "Papel", "Tijeras"};
+        Random rand = new Random();
+        int eleccionComputadora = rand.nextInt(3); // 0 para Piedra, 1 para Papel, 2 para Tijeras
+
+        String eleccionComputadoraTexto = opciones[eleccionComputadora];
+
+        String resultado = determinarResultado(eleccionJugador, eleccionComputadoraTexto);
+
+        // Mostrar el resultado en el TextView
+        txtResultado.setText(resultado);
+    }
+
+    private String determinarResultado(String eleccionJugador, String eleccionComputadora) {
+        if (eleccionJugador.equals(eleccionComputadora)) {
+            return "Empate, la máquina tambien sacó "+eleccionComputadora;
+        } else if ((eleccionJugador.equals("Piedra") && eleccionComputadora.equals("Tijeras")) ||
+                (eleccionJugador.equals("Papel") && eleccionComputadora.equals("Piedra")) ||
+                (eleccionJugador.equals("Tijeras") && eleccionComputadora.equals("Papel"))) {
+            return "Ganaste, la máquina sacó "+ eleccionComputadora;
+        } else {
+            return "Perdiste, la máquina sacó "+ eleccionComputadora;
+        }
     }
 }
